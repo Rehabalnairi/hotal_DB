b8a84e3fe74a7f22ea06b7e335f9959b91ed236d
Rehabalnairi <133367687+Rehabalnairi@users.noreply.github.com>
2025-08-07 14:35:59 +0400

    update class

--- Repositories/RoomRepostory.cs
+++ Repositories/RoomRepostory.cs
@@ -3,10 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using hotal_DB.Data;
+using hotal_DB.Models;
 
 namespace hotal_DB.Repositories
 {
     class RoomRepostory
+
     {
+        private readonly HotelContext _context;
+        public RoomRepostory(HotelContext context)
+        {
+            _context = context;
+        }
+
+        // Add a new room
+        public void AddRoom(Room room)
+        {
+            _context.Rooms.Add(room);
+            _context.SaveChanges();
+        }
+        // Get all rooms
+        public List<Room> GetAllRooms()
+        {
+            return _context.Rooms.ToList();
+        }
+        // Get room by ID
+        public Room GetRoomById(int roomId)
+        {
+            return _context.Rooms.Find(roomId);
+        }
+        // Update a room
+        public void UpdateRoom(Room room)
+        {
+            _context.Rooms.Update(room);
+            _context.SaveChanges();
+        }
+        // Delete a room
+        public void DeleteRoom(int roomId)
+        {
+            var room = _context.Rooms.Find(roomId);
+            if (room != null)
+            {
+                _context.Rooms.Remove(room);
+                _context.SaveChanges();
+            }
+        }
+        // Get rooms by type
+        public List<Room> GetRoomsByType(string roomType)
+        {
+            return _context.Rooms.Where(r => r.RoomType == roomType).ToList();
+        }
     }
 }
